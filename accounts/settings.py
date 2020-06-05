b3ef5a6925884605272bc67dbd9507d3d34f2a55
gk-svg <gk32239@gmail.com>
2020-06-05 07:04:29 +0530

    heroku deployement settings changed

--- accounts/settings.py
+++ accounts/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL="index"
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
