9294abcc0f41256a8fba9c325e67dfc80e88a3e9
gk-svg <gk32239@gmail.com>
2020-06-05 07:37:58 +0530

    database settings changed

--- accounts/settings.py
+++ accounts/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 django_heroku.settings(locals())
+import dj_database_url
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
@@ -82,8 +85,12 @@ WSGI_APPLICATION = 'accounts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'accounts',
+        'USER': 'postgres',
+        'PASSWORD': 'gautam2520032',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
